b25071cdecfa922e13e524d3a1d09327d3ed2b2d
XyHan <xyhan_1@hotmail.com>
2021-10-20 18:43:48 +0200

    feat(controller): add commandBus to index controller with POST add action

--- src/Ui/Controller/IndexController.php
+++ src/Ui/Controller/IndexController.php
@@ -2,11 +2,14 @@
 
 namespace App\Ui\Controller;
 
+use App\Application\Command\MyCommand\MyCommand;
 use App\Application\Query\MyQuery\MyQuery;
-use App\Application\Query\MyQuery\MyQueryHandler;
+use App\Domain\Command\CommandBus;
 use App\Domain\Query\QueryBus;
+use Ramsey\Uuid\Uuid;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -21,17 +24,24 @@ class IndexController extends AbstractController
     private QueryBus $queryBus;
 
     /**
+     * @var CommandBus
+     */
+    private CommandBus $commandBus;
+
+    /**
      * @var SerializerInterface
      */
     private SerializerInterface $serializer;
 
     /**
      * @param QueryBus $queryBus
+     * @param CommandBus $commandBus
      * @param SerializerInterface $serializer
      */
-    public function __construct(QueryBus $queryBus, SerializerInterface $serializer)
+    public function __construct(QueryBus $queryBus, CommandBus $commandBus, SerializerInterface $serializer)
     {
         $this->queryBus = $queryBus;
+        $this->commandBus = $commandBus;
         $this->serializer = $serializer;
     }
 
@@ -41,6 +51,20 @@ class IndexController extends AbstractController
     public function index(): JsonResponse
     {
         return new JsonResponse('Hello !');
+    }
+
+    /**
+     * @Route("/add", name="add", methods={"POST"})
+     */
+    public function add(Request $request): JsonResponse
+    {
+        $command = new MyCommand(
+            $request->request->get('requestId'),
+            Uuid::uuid4(),
+            $request->request->get('uuid')
+        );
+        $this->commandBus->dispatch($command);
+        return new JsonResponse($this->serializer->serialize($command, 'json'));
     }
 
     /**
